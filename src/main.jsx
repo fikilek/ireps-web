3a064722bc9ec5134e9a1d0b63ce28e61434d064
fikile kentane <fikilekentane@gmail.com>
2026-04-26 10:33:39 +0200

    Mount geo and warehouse providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthProvider.jsx";
+import { GeoProvider } from "@/context/GeoContext.jsx";
+import { WarehouseProvider } from "@/context/WarehouseContext.jsx";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <AuthProvider>
-        <App />
+        <GeoProvider>
+          <WarehouseProvider>
+            <App />
+          </WarehouseProvider>
+        </GeoProvider>
       </AuthProvider>
     </Provider>
   </StrictMode>,
